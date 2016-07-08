08d8e7b29b7b8d9d4262a75d4db4a327e9388b8c
Mathias Buus <mathiasbuus@gmail.com>
2016-07-07 17:12:20 -0700

    add cache

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var uint64be = require('uint64be')
+var lru = require('lru')
 
 module.exports = Tree
 
@@ -8,12 +9,18 @@ function Tree (storage, opts) {
 
   this.storage = storage
   this.live = !!opts.live
+  this.cache = lru(opts.cacheSize || 512)
 }
 
 Tree.prototype.get = function (i, cb) {
+  var k = '' + i
+  var val = this.cache.get(k)
+  if (val) return cb(null, val)
+
   var leaf = this.live && !(i & 1)
   var offset = 40 * i + 32 * (this.live ? Math.ceil(i / 2) : 0)
   var length = leaf ? 72 : 40
+  var self = this
 
   this.storage.read(offset, length, function (err, buf) {
     if (err) return cb(err)
@@ -23,12 +30,9 @@ Tree.prototype.get = function (i, cb) {
 
     if (!size && isNil(hash)) return cb(new Error('Index not found'))
 
-    cb(null, {
-      index: i,
-      hash: hash,
-      size: size,
-      signature: leaf ? buf.slice(40) : null
-    })
+    val = new Node(i, hash, size, leaf ? buf.slice(40) : null)
+    self.cache.set(k, val)
+    cb(null, val)
   })
 }
 
@@ -43,10 +47,19 @@ Tree.prototype.put = function (i, node, cb) {
   node.hash.copy(buf, 0)
   uint64be.encode(node.size, buf, 32)
   if (leaf) node.signature.copy(buf, 40)
+
+  this.cache.set('' + i, new Node(i, node.hash, node.size, node.signature || null))
   this.storage.write(offset, buf, cb)
 }
 
 function noop () {}
+
+function Node (i, hash, size, sig) {
+  this.index = i
+  this.hash = hash
+  this.size = size
+  this.signature = sig
+}
 
 function isNil (buf) {
   for (var i = 0; i < buf.length; i++) {
@@ -54,4 +67,3 @@ function isNil (buf) {
   }
   return true
 }
-
